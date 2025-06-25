f7727249625a2e4dfa4b33fa288d470c49a649ad
ihorhnennyi <ihorhnennyi@gmail.com>
2025-06-25 14:39:38 +0300

    chore: setup project structure, installed dependencies, connected MongoDB, added .env and base config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI') || '',
+      }),
+    }),
+  ],
 })
 export class AppModule {}
