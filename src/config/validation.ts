2716d944f0d87920feeed536a52a82ef0565918c
ihorhnennyi <ihorhnennyi@gmail.com>
2025-07-08 13:56:03 +0300

    Добавлена валидация переменных окружения: JWT, MongoDB, Google Drive, ROOT_ADMIN

--- src/config/validation.ts
+++ src/config/validation.ts
@@ -10,7 +10,17 @@ export const validationSchema = Joi.object({
   JWT_SECRET: Joi.string().required().messages({
     'any.required': '"JWT_SECRET" обязателен!',
   }),
-
   JWT_ACCESS_EXPIRES_IN: Joi.string().default('15m'),
   JWT_REFRESH_EXPIRES_IN: Joi.string().default('30d'),
+
+  ROOT_ADMIN_EMAIL: Joi.string().email().required(),
+  ROOT_ADMIN_PASSWORD: Joi.string().min(6).required(),
+
+  GOOGLE_KEY_FILE: Joi.string().required(),
+  GOOGLE_DRIVE_SHARE_EMAIL: Joi.string().email().required(),
+  GOOGLE_DRIVE_OWNER: Joi.string().email().required(),
+  GOOGLE_SERVICE_EMAIL: Joi.string().email().required(),
+  GOOGLE_DRIVE_PARENT_FOLDER_ID: Joi.string().required(),
+
+  FRONTEND_URL: Joi.string().uri().default('http://localhost:5173'),
 });
