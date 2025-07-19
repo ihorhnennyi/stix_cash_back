d62a799a5c462327221534d282470597a60c01c7
ihorhnennyi <ihorhnennyi@gmail.com>
2025-07-19 11:44:03 +0300

    chore(cors): enable global CORS support for all origins with credentials

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://triakwebsite.xyz'],
+    origin: true,
     credentials: true,
   });
 
