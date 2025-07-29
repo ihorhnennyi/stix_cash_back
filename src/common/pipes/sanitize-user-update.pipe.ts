b181cf4dc052f7ca3bb5f0ed8bc86d7d586a9450
ihorhnennyi <ihorhnennyi@gmail.com>
2025-07-29 13:06:14 +0300

    fix(user): allow balance fields to pass through SanitizeDtoPipe

--- src/common/pipes/sanitize-user-update.pipe.ts
+++ src/common/pipes/sanitize-user-update.pipe.ts
@@ -3,8 +3,8 @@ import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 @Injectable()
 export class SanitizeDtoPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
-    delete value.balance;
-    delete value.balanceBTC;
+    // delete value.balance;
+    // delete value.balanceBTC;
 
     return value;
   }
